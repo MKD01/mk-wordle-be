56a65b48b4ea57c248b8faffb6185d380b4e78b5
mkd <kabir.dastgir@hotmail.com>
2025-07-25 10:37:13 +0100

    add basic error handling

--- _tests_/app.test.js
+++ _tests_/app.test.js
@@ -14,6 +14,17 @@ describe("/api", () => {
   });
 });
 
+describe("api/unknown/endpoint", () => {
+  it("should respond with 404 page not found, for any invalid endpoint", () => {
+    return request(app)
+      .get("/api/unknown")
+      .expect(404)
+      .then(({ body }) => {
+        expect(body.msg).toBe("Page Not Found");
+      });
+  });
+});
+
 describe("/api/word/daily", () => {
   it("should respond with a 5 letter daily word", () => {
     return request(app)
