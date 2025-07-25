8839091483a173dc56a1a8b9cf6b534206cfae65
mkd <kabir.dastgir@hotmail.com>
2025-07-25 10:13:53 +0100

    update /api to follow MVC pattern

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { getEndpoints } = require("./models/api.model");
+const { getEndpoints } = require("./controllers/api.controller");
 
 const app = express();
 
